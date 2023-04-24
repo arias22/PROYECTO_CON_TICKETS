360aed000b4262eb5be5e923129c5589ad45a230
lucas <lucas2002.vg@gmail.com>
2023-04-24 20:38:56 +0200

    plan_de_pruebas

--- prueba.sh
+++ prueba.sh
@@ -9,20 +9,81 @@ gcc main_v3.c -o main_v3
 # Compilar receptor
 gcc receptor_v3.c -o receptor_v3
 
-# Define la lista de programas y sus argumentos
+# Define la lista de programas y sus argumentos 
+#0-> Pagos y Anulaciones 1->Reservas 2->Administración 3->Consultas
+#Prueba 1
 programas_main=(
-  "./main_v3 0 4 2"
-  "./main_v3 1 4 1"
-  "./main_v3 2 4 1"
-  "./main_v3 3 4 1"
+  "./main_v3 0 2 0"
+  "./main_v3 0 2 2"
+  "./main_v3 0 2 2"
+  "./main_v3 1 2 0"
+  "./main_v3 1 2 3"
 )
 
 programas_receptor=(
-"./receptor_v3 0 4"
-  "./receptor_v3 1 4"
-  "./receptor_v3 2 4"
-  "./receptor_v3 3 4"
+"./receptor_v3 0 2"
+ "./receptor_v3 1 2"
+
   )
+
+# Prueba 2
+#   programas_main=(
+#   "./main_v3 0 2 1"
+#   "./main_v3 0 2 0"
+#   "./main_v3 0 2 0"
+#   "./main_v3 1 2 2"
+#   "./main_v3 1 2 1"
+#   "./main_v3 1 2 0"
+# )
+
+# programas_receptor=(
+# "./receptor_v3 0 2"
+#  "./receptor_v3 1 2"
+
+#   )
+
+
+# Prueba 3
+#   programas_main=(
+#   "./main_v3 0 3 3"
+#   "./main_v3 0 3 3"
+#   "./main_v3 0 3 3"
+#   "./main_v3 1 3 3"
+#   "./main_v3 1 3 3"
+#   "./main_v3 1 3 3"
+#   "./main_v3 2 3 3"
+#   "./main_v3 2 3 0"
+#   "./main_v3 2 3 3"
+
+# )
+
+# programas_receptor=(
+# "./receptor_v3 0 3"
+#  "./receptor_v3 1 3"
+#  "./receptor_v3 2 3"
+
+#   )
+
+# Prueba 4
+#   programas_main=(
+#   "./main_v3 0 4 1"
+#   "./main_v3 0 4 0"
+#   "./main_v3 1 4 3"
+#   "./main_v3 2 4 0"
+#   "./main_v3 2 4 0"
+#   "./main_v3 2 4 1"
+#   "./main_v3 3 4 3"
+#   "./main_v3 3 4 3"
+
+# )
+
+# programas_receptor=(
+# "./receptor_v3 0 4"
+#  "./receptor_v3 1 4"
+#  "./receptor_v3 2 4"
+#  "./receptor_v3 3 4"
+
+#   )
 i=1
 j=1
 
